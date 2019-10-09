8f5ce9ee70cc74bad1536cfb151ae4ebd098e7e3
AmirFarhan <mirmirfarhan97@gmail.com>
2019-10-09 16:16:12 +0800

    Go 2 weeks deep

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function getGribData(targetMoment, hourOffset){
 		// only go 2 weeks deep
 		var newDate = moment(targetMoment.startOf('day'), "DD-MM-YYYY").add(hourOffset, 'hours');
 		console.log(newDate.diff(targetMoment, 'days'));
-		if (newDate.diff(targetMoment, 'days') > 0){
+		if (newDate.diff(targetMoment, 'days') > 15){
 			console.log('hit limit, harvest complete or there is a big gap in data..');
 			process.exit(0);
             return;
